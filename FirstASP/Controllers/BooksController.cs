bf1c40de6a90dab75a79419e875cab4302555680
Karim Abdullin <abdullinkarim420@gmail.com>
2025-07-23 17:26:48 +0300

    first commit

---
+++ FirstASP/Controllers/BooksController.cs
@@ -0,0 +1,88 @@
+﻿using FirstASP.Data;
+using FirstASP.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstASP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly FirstAPIContext _context;
+
+        public BooksController(FirstAPIContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Book>>> GetBooks()
+        {
+            return Ok(await _context.books.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBookById(int id)
+        {
+            var book = await _context.books.FindAsync(id);
+
+            if (book == null)
+                return NotFound();
+
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Book>> AddBook(CreateBookDto bookDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var book = new Book
+            {
+                Title = bookDto.Title,
+                Author = bookDto.Author,
+                YearPublished = bookDto.YearPublished
+            };
+
+            _context.books.Add(book);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetBookById), new { id = book.Id }, book);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateBook(int id, Book updatedBook)
+        {
+            var book = await _context.books.FindAsync(id);
+
+            if (book == null)
+                return NotFound();
+
+            book.Id = updatedBook.Id;
+            book.Title = updatedBook.Title;
+            book.Author = updatedBook.Author;
+            book.YearPublished = updatedBook.YearPublished;
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBook(int id)
+        {
+            var book = await _context.books.FindAsync(id);
+
+            if (book == null)
+                return NotFound();
+
+            _context.books.Remove(book);
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ FirstASP/Models/Book.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FirstASP.Models
+{
+    public class Book
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [StringLength(100)]
+        public string Title { get; set; } = null!;
+
+        [StringLength(50)]
+        public string Author { get; set; } = null!;
+
+        [Range(0, 2100)]
+        public int YearPublished { get; set; }
+    }
+    public class CreateBookDto
+    {
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Author { get; set; }
+
+        [Range(0, 2100)]
+        public int YearPublished { get; set; }
+    }
+}
